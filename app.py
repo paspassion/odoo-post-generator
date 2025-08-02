7d01f53539c694b2b94a4db403320db00824bffb
paspassion <contact@passion-prevention.com>
2025-08-02 11:22:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,8 @@ def genere_post():
 if __name__ == '__main__':
     import os
 
-port = int(os.environ.get("PORT", 5000))  # Render utilise cette variable automatiquement
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
