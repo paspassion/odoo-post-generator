5c2e55f8f6522a14029611554d689e19e6c0179c
paspassion <contact@passion-prevention.com>
2025-08-02 11:27:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,9 +60,6 @@ def genere_post():
     return jsonify({"postages": postages})
 
 if __name__ == '__main__':
-    import os
-
-if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
